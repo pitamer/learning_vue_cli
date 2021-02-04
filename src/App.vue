0a43e268c78cec4fb119d842d76a9a26f690bebe
pitamer <itamar@codicate.com>
2021-02-04 22:09:50 +0200

    Start removing components and routes that are not needed anymore

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <!-- <router-link to="/about">About</router-link> | -->
       <router-link to="/lists">Lists</router-link>
     </div>
     <router-view />
--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,12 @@ const routes = [
     name: "Home",
     component: Home,
   },
-  {
-    path: "/about",
-    name: "About",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "@/views/About.vue"),
-  },
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "@/views/About.vue"),
+  // },
   {
     path: "/lists",
     name: "Lists",
